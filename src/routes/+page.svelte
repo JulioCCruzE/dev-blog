e727d4f98a9a1f62e61ccbd72c6d6e5d57e4a59b
JulioCCruzE <jc.shogun.t6s@gmail.com>
2024-07-09 11:52:18 -0600

    update file to load post header and post listing

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,24 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import HomeHeader from '$lib/components/HomeHeader.svelte';
+	import PostListing from '$lib/components/PostListing.svelte';
+	import type { PageData } from './$types';
+
+	export let data: PageData;
+</script>
+
+<HomeHeader />
+
+<section class="pb-6">
+	<header
+		class="inline-block border-b dark:border-gray-700
+    py-2 mx-4 mb-4"
+	>
+		<h2 class="text-3xl font-semibold">Blog Posts</h2>
+	</header>
+
+	<section class="divide-y dark:divide-gray-700">
+		{#each data.posts as post}
+			<PostListing {post} />
+		{/each}
+	</section>
+</section>
