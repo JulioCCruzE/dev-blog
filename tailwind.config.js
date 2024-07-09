61aea5e3dfd40bc32ede3f35617b3b50509aef7f
JulioCCruzE <jc.shogun.t6s@gmail.com>
2024-07-09 11:56:15 -0600

    add configuration files

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+	darkMode: 'class',
+	theme: {
+		extend: {
+			colors: {
+				'dark-background': '#0F161E'
+			}
+		}
+	},
+	plugins: [require('@tailwindcss/typography'), require('@tailwindcss/aspect-ratio')]
+};
