67a01313024e46669e6710d27e56c9be18496a87
JulioCCruzE <jc.shogun.t6s@gmail.com>
2024-07-09 12:05:13 -0600

    add principal code for publications

---
+++ src/routes/publications/+page.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import PublicationsHeader from '$lib/components/PublicationsHeader.svelte';
+	import PublicationsListing from '$lib/components/PublicationsListing.svelte';
+	// import RoboticsListing from '$lib/components/RoboticsListing.svelte';
+	import type { PageData } from '../$types';
+
+	export let data: PageData;
+</script>
+
+<PublicationsHeader />
+
+<section class="pb-6">
+	<header
+		class="inline-block border-b dark:border-gray-700
+    py-2 mx-4 mb-4"
+	>
+		<h2 class="text-3xl font-semibold">Papers</h2>
+	</header>
+
+	<section class="divide-y dark:divide-gray-700">
+		{#each data.posts as post}
+			<PublicationsListing {post} />
+		{/each}
+	</section>
+</section>
