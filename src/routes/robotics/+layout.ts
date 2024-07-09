da601e02debff9fe9b62f0496afc0d830b71b8ba
JulioCCruzE <jc.shogun.t6s@gmail.com>
2024-07-09 12:04:40 -0600

    add principal code for robotics

---
+++ src/routes/robotics/+layout.ts
@@ -0,0 +1,14 @@
+// import type { LayoutLoad } from './$types';
+import type { LayoutLoad } from '../$types';
+
+// add this to root layout
+// to prerender all our pages
+// during build time
+export const prerender = true;
+
+// get url path whenever visiting a new page
+export const load: LayoutLoad = ({ url }) => {
+	return {
+		pathname: url.pathname
+	};
+};
---
+++ src/routes/robotics/+page.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	// import HomeHeader from '$lib/components/HomeHeader.svelte';
+	import RoboticsHeader from '$lib/components/RoboticsHeader.svelte';
+	import RoboticsListing from '$lib/components/RoboticsListing.svelte';
+	import type { PageData } from '../$types';
+
+	export let data: PageData;
+</script>
+
+<RoboticsHeader />
+
+<section class="pb-6">
+	<header
+		class="inline-block border-b dark:border-gray-700
+    py-2 mx-4 mb-4"
+	>
+		<h2 class="text-3xl font-semibold">Videos</h2>
+	</header>
+
+	<section class="divide-y dark:divide-gray-700">
+		{#each data.posts as post}
+			<RoboticsListing {post} />
+		{/each}
+	</section>
+</section>
