e325c9a6aa87dc631101ce2b11848a5bf2d9b9ce
JulioCCruzE <jc.shogun.t6s@gmail.com>
2024-07-10 12:44:15 -0600

    add href to publications for fix build error

--- src/lib/components/PublicationsListing.svelte
+++ src/lib/components/PublicationsListing.svelte
@@ -4,7 +4,7 @@
 
 	export let post: MarkdownPostMetadataAndSlug;
 
-	// let href = `/blog/publications/${post.slug}`;
+	let href = `/blog/publications/${post.slug}`;
 	// let youtubeHref = post.metadata.youtubeId ? `https://youtu.be/${post.metadata.youtubeId}` : '';
 </script>
 
@@ -16,24 +16,23 @@
 	<!-- </a> -->
 
 	<div class="flex-1 py-2 sm:py-0">
-		<!-- <a {href}> -->
-		<p class="py-2 font-light">
-			{post.metadata.author}
-		</p>
-		<h3 class="text-xl font-medium mb-1">
-			{post.metadata.title}
-		</h3>
-		<p class="py-2 font-light">
-			{post.metadata.summary}
-		</p>
-		<p class="font-light text-gray-60 dark:text-gray-300">
-			<span>Published:</span>
-			<time datetime={post.metadata.publishedAt}>
-				{formatPublishedAt(post.metadata.publishedAt)}
-			</time>
-		</p>
-
-		<!-- </a> -->
+		<a {href}>
+			<p class="py-2 font-light">
+				{post.metadata.author}
+			</p>
+			<h3 class="text-xl font-medium mb-1">
+				{post.metadata.title}
+			</h3>
+			<p class="py-2 font-light">
+				{post.metadata.summary}
+			</p>
+			<p class="font-light text-gray-60 dark:text-gray-300">
+				<span>Published:</span>
+				<time datetime={post.metadata.publishedAt}>
+					{formatPublishedAt(post.metadata.publishedAt)}
+				</time>
+			</p>
+		</a>
 		{#if post.metadata.PDFlink}
 			<p>
 				Download: <a href={post.metadata.PDFlink} class="p-2 text-purple-800 dark:text-yellow-200"
